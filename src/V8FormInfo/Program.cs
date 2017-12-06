e0bfa8731aab9913b705c7f327d1b7e5669df8c0
s.marusov <s.marusov@gmail.com>
2017-12-07 01:35:05 +0300

    Каркас консольной программы для вывода информации о данных контейнера обычной формы

---
+++ src/V8FormInfo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace V8FormInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int argsCount = args.Length;
+            
+            if (argsCount == 0)
+            {
+                Console.WriteLine("Вывод информации о составе контейнера обычной формы 1С:Предприятие 8");
+            }
+        }
+    }
+}
