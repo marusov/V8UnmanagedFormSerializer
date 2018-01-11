2136b54dfe23d9fd81db85feeda3d04703a918f6
s.marusov <s.marusov@gmail.com>
2018-01-12 00:55:31 +0300

    Работа с BinaryReader

--- src/V8FormInfo/Program.cs
+++ src/V8FormInfo/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace V8FormInfo
 {
@@ -8,10 +10,39 @@ namespace V8FormInfo
         {
             int argsCount = args.Length;
             
-            if (argsCount == 0)
+            if (argsCount != 1)
             {
                 Console.WriteLine("Вывод информации о составе контейнера обычной формы 1С:Предприятие 8");
             }
+            else
+            {
+                FormReader.Read(args[0]);
+            }
+        }
+    }
+
+    class FormReader
+    {
+        public static void Read(string filePath)
+        {
+            List<int> blockHeader = new List<int>();
+            
+            Console.WriteLine($"Имя файла: {filePath}");
+            using (BinaryReader reader = new BinaryReader(File.Open(filePath, FileMode.Open)))
+            {
+                blockHeader.Add(reader.ReadInt32());
+                blockHeader.Add(reader.ReadInt32());
+                blockHeader.Add(reader.ReadInt32());
+                blockHeader.Add(reader.ReadInt32());
+
+            }
+            
+            Console.WriteLine($"Маркер: {blockHeader[0]}");
+            Console.WriteLine($"Размер: {blockHeader[1]}");
+            Console.WriteLine($"Версия: {blockHeader[2]}");
+            Console.WriteLine($"Резерв: {blockHeader[3]}");
+
+
         }
     }
 }
